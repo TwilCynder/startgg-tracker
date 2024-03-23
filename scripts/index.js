eb34f80215e4174d87cc89fd27066228a6fd7884
TwilCynder <twilcynder@gmail.com>
2024-03-23 20:20:21 +0100

    Added mode selection to the homepage

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,60 @@ import { testTokenFactory } from "./lib/api/testToken.js"
 
 let testToken = await testTokenFactory();
 
+
+
+function hide(elementName){
+    let element = document.querySelector(elementName);
+    
+    element.style.setProperty(`display`, "none");
+    element.style.setProperty('opacity', "0");
+}
+
+function show(elementName){let element = document.querySelector(elementName);
+    
+    element.style.setProperty(`display`, "flex");
+    element.style.setProperty('opacity', "100%");
+}
+
+
+class ToggleManager {
+    #ids;
+    #currentIdx = 0;
+
+    /**
+     * @param {...string} elementsIDs 
+     */
+    constructor(...elementsIDs){
+        this.#ids = elementsIDs;
+    }
+
+    #update(){
+        for (let i = 0; i < this.#ids.length; i++){
+            if (this.#currentIdx == i){
+                hide(this.#ids[i]);
+            } else {
+                show(this.#ids[i]);
+            }
+        }
+    }
+
+    setIndex(i){
+        this.#currentIdx = i;
+        this.#update();
+    }
+}
+
+
+new ToggleManager("#token-input", "#mode-select");
+
+function displayMainMenu(){
+    hide("#token-input");
+    show("#mode-select");
+}
+
 document.getElementById("start-button").addEventListener("click", async (element) => {
+    displayMainMenu();
+    /*
     let token = document.getElementById("apikey").value;
 
     let res = await testToken(token);
@@ -19,6 +72,15 @@ document.getElementById("start-button").addEventListener("click", async (element
     }
 
     localStorage.setItem('token', token);
+    */
+})
 
-    window.location.href = "./event_sets.html"
+document.querySelector("#event-mode .button").addEventListener("click", async (element) => {
+    let slug = document.querySelector("#event-mode .mode-area-input").value;
+    console.log(slug);
+    //window.location.href = "./event_sets.html?slug=" 
+})
+
+document.querySelector("#player-mode .button").addEventListener("click", async (element) => {
+    alert("Ce mode n'est pas encore impémenté ! désooooooo")
 })
