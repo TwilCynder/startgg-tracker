25513f5c243c42bdf7335bdac3d51fc3f30e5079
TwilCynder <twilcynder@gmail.com>
2024-08-28 18:48:58 +0200

    small fixes

--- scripts/index.js
+++ scripts/index.js
@@ -69,6 +69,7 @@ function eventModeGOCallback(){
     slug = processEventSlug(slug);
     if (!slug){
         alert("Please enter a valid event URL. Go to the page of your event on start.gg and copy the content of the URL bar.");
+        return;
     }
 
     window.location.href = "./event_sets.html?event=" + slug 
@@ -85,7 +86,7 @@ function playerModeGOCallback(){
     alert("Ce mode n'est pas encore impémenté ! désooooooo")
 }
 
-document.querySelector("#player-mode .button").addEventListener("click", async (element) => playerModeGOCallback);
+document.querySelector("#player-mode .button").addEventListener("click", playerModeGOCallback);
 document.querySelector("#player-mode .mode-area-input").addEventListener("keydown", (event) => {
     if (event.code == "Enter"){
         playerModeGOCallback()
