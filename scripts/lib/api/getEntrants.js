d5ede738e40a1e53a6e79a042c1ce49ed9c12a7e
TwilCynder <twilcynder@gmail.com>
2024-09-11 17:11:46 +0200

    getEntrants uses QueryManager

--- scripts/lib/api/getEntrants.js
+++ scripts/lib/api/getEntrants.js
@@ -1,13 +1,10 @@
 import { deep_get } from "../util.js";
-import { loadQuery } from "./sgg-helper.js";
+import queryManager from "../queryManager.js";
 
 const schema_filename = "./schemas/EventEntrants.graphql";
-const default_tries = 3;
 
-
-
-export async function getEventEntrantsFactory(maxTries = default_tries){
-    let query = await loadQuery(new URL(schema_filename, import.meta.url), maxTries);
+export async function getEventEntrantsFactory(){
+    let query = await queryManager.tryQuery("eventEntrants", new URL(schema_filename, import.meta.url));
 
     return async function getEventEntrants(slug, client, limiter){
         let response = await query.execute(client, {slug}, limiter);
