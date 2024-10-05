9329966371bf5ea9bfac1fb9a9175638af635804
TwilCynder <twilcynder@gmail.com>
2024-10-05 02:03:46 +0200

    rematch calculation

--- scripts/lib/api/getUserSets.js
+++ scripts/lib/api/getUserSets.js
@@ -5,13 +5,13 @@ const schema_filename = "./schemas/UserSets.graphql"
 export async function getUserSetsFactory(){
     let query = await queryManager.tryQuery("userSets", new URL(schema_filename, import.meta.url));
 
-    return async function getUserSets(slug, after, client, limiter){
-        let sets = await query.executePaginated(client, {slug, after}, "user.player.sets", limiter, {
+    return async function getUserSets(id, after, client, limiter){
+        let sets = await query.executePaginated(client, {id, after}, "user.player.sets", limiter, {
             perPage: 90
         });
 
         if (!sets){
-            throw new Error("Couldn't fetch sets from " + slug + " ; invalid response (might indicate non-existent user)");
+            throw new Error("Couldn't fetch sets from " + id + " ; invalid response (might indicate non-existent user)");
         }
 
         return sets;
--- scripts/lib/check_rematches.js
+++ scripts/lib/check_rematches.js
@@ -5,6 +5,14 @@ import { deep_get } from "./util.js";
 const getEventEntrants = await getEventEntrantsFactory();
 const getUserSets = await getUserSetsFactory();
 
+class Player {
+    constructor(user, player){
+        this.slug = user.slug;
+        this.id = user.id;
+        this.name = player.gamerTag;
+    }
+}
+
 /**
  * 
  * @param {SGGHelperClient} client 
@@ -14,26 +22,90 @@ const getUserSets = await getUserSetsFactory();
  */
 export async function get_rematches(client, slug, after, limiter){
     let entrantsList = await getEventEntrants(slug, client, limiter);
-    let slugs = entrantsList.map(entrant => {
+    console.log(entrantsList.length)
+    let players = entrantsList.map(entrant => {
         let p = entrant.participants;
-        console.log(entrantsList.length)
         if (!p || p.length != 1) return;
-        let slug = deep_get(p, "0.user.slug");
-        if (!slug) {
-            console.warn("User with no slug !");
-            return;
+        p = p[0];
+        let user = p.user;
+        let player = p.player;
+        if (!user){
+            console.warn("Participant with no user :", player.gamerTag);
         }
-        return slug;
+        return new Player(user, player);
     }).filter(slug => !!slug);
-    let sets = await Promise.all(slugs.map( async slug => ({sets: await getUserSets(slug, after, client, limiter), slug})));
+    let sets = await Promise.all(players.slice(0, 20).map( async player => ({sets: await getUserSets(player.id, after, client, limiter), player})));
     console.log(sets);
+    getRematchesList(buildMatchesMatrix(sets));
 }
 
 /**
  * 
  * @param {string[]} slugs 
- * @param {Array<Array<any>>} setsLists 
+ * @param {Array<{sets: Array<{}>, player: Player}>} playersLists 
  */
-function processData(slugs, setsLists){
+function buildMatchesMatrix(playersLists){
+    let indexes = playersLists.reduce((prev, current, index) => {
+        prev[current.player.id] = index;
+        return prev;
+    }, {});
 
+    let earliest = Infinity
+    let res = playersLists.map((playerData, currentIndex) => {
+        let h2h = Array(playersLists.length - currentIndex - 1).fill(null).map(_ => ([]));
+        let id = playerData.player.id;
+        
+        for (let set of playerData.sets){
+            console.log(set.completedAt) ; if (set.completedAt < 1722513915) {
+                console.error("TMRLPT STARTGG", set);
+                if (set.completedAt && set.completedAt < earliest){
+                    earliest = set.completedAt;
+                }
+            };
+            let currentPlayerSlotIndex = null;
+            if (set.slots[0].entrant.participants.length > 1) continue; //2v2
+            for (let i = 0; i < 2; i++){
+                let p = set.slots[i].entrant.participants[0]
+                if (!p.user) continue;
+                if (p.user.id == id){
+                    currentPlayerSlotIndex = i;
+                }
+            }
+            if (currentPlayerSlotIndex === null){
+                console.error("PLAYER NOT FOUND IN OWN SET", set, id, playerData.player.slug);
+                continue;
+            }
+            let otherPlayerID = deep_get(set, `slots.${1 - currentPlayerSlotIndex}.entrant.participants.0.user.id`);
+            if (otherPlayerID === null){
+                console.warn("Other play doesn't have a user ID", set, id, playerData.player.slug);
+                continue;
+            }
+            let otherPlayerEntrantIndex = indexes[otherPlayerID];
+            if (otherPlayerEntrantIndex){
+                if (otherPlayerEntrantIndex > currentIndex){
+                    h2h[otherPlayerEntrantIndex - currentIndex - 1].push(set);
+                }
+            }
+        }
+        return {player: playerData.player, h2h}
+    })
+    console.warn(earliest)
+    return res;
+}
+
+/**
+ * @param {ReturnType<buildMatchesMatrix>} matrix 
+ */
+function getRematchesList(matrix){
+    for (let p of matrix){
+        console.log(p.player.name)
+    }
+    matrix.forEach((playerData, lineIndex) => {
+        playerData.h2h.forEach((matches, colIndex) => {
+            if (matches.length > 0){
+                let otherPlayerData = matrix[lineIndex + colIndex + 1];
+                console.log(playerData.player.name, otherPlayerData.player.name, matches.length, matches);
+            }
+        })
+    })
 }
