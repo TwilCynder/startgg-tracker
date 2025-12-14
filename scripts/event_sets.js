ca56b7edf898aa69edc2809de9d8308c13c0d547
TwilCynder <twilcynder@gmail.com>
2025-12-14 01:45:55 +0100

    we don't need that many js fiels

--- scripts/event_sets.js
+++ scripts/event_sets.js
@@ -1,8 +1,41 @@
 
 import { SGGHelperClient } from "./lib/api/sgg-helper.js";
 import { initLayout } from "./lib/sets_display.js";
-import { loadEventSets } from "./lib/loadEventSets.js";
 import { processEventSlug } from "./lib/util.js";
+import { getCalledSetsFactory } from "./lib/api/getCalledSets.js";
+import { resetContent, fitTexts, addSet } from "./lib/sets_display.js";
+const getCalledSets = await getCalledSetsFactory();
+
+export async function loadEventSets(client, slug, config){
+
+    let event = await getCalledSets(slug, client);
+
+    if (!event);
+
+    resetContent();
+
+    //test
+    //event.sets.nodes = Array(5).fill(event.sets.nodes).flat()
+
+    let colsN = Math.ceil(Math.sqrt(event.sets.nodes.length));
+    console.log("colsn", colsN);
+
+    if (window.screen.width > window.screen.height){
+        document.querySelector(".content").style.setProperty("grid-template-columns", "1fr ".repeat(colsN))
+    } else {
+        document.querySelector(".content").style.setProperty("grid-template-columns", "1fr " + (event.sets.nodes.length > 4 ? "1fr" : ""))
+    }
+    
+
+    let i = 0;
+    for (let set of event.sets.nodes){
+        console.log(set.state == 2 ? "Started" : "Called", set.slots[0].entrant.name, set.slots[1].entrant.name)
+        addSet(set, i, config);
+        i++;
+    }
+    fitTexts(i);
+
+}
 
 let config = await fetch("../config.json")
     .then(response => response.json())
