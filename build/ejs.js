cf72aa9e043220512ed96eedc32b7a8d5edfd307
TwilCynder <twilcynder@gmail.com>
2026-06-08 20:07:30 +0200

    Improved template rendering (no longer require an explicit list)

--- build/ejs.js
+++ build/ejs.js
@@ -1,16 +1,20 @@
 import ejs from "ejs";
-import fs from "node:fs";
+import fs from "fs/promises"
 
 const path = (process.argv[2] ?? ".");
 
-async function renderFile(template){
-    let filename = `${path}/pages/templates/${template}.ejs`
-    console.log(filename);
-    let res = await ejs.renderFile(filename);
-    fs.writeFileSync(`${path}/pages/rendered/${template}.html`, res);
+async function renderFile(filename){
+    let templatePath = `${path}/pages/templates/${filename}`
+    console.log("Rendering", templatePath);
+    let res = await ejs.renderFile(templatePath);
+    await fs.writeFile(`${path}/pages/rendered/${filename.replace("ejs", "html")}`, res);
 }
 
-let lines = fs.readFileSync(new URL(`./templates.txt`, import.meta.url)).toString('utf-8').replaceAll(/\r/g, '').split('\n');
-console.log("Rendering EJs templates :", ...lines);
-lines.forEach(line => renderFile(line));
+//let lines = fs.readFileSync(new URL(`./templates.txt`, import.meta.url)).toString('utf-8').replaceAll(/\r/g, '').split('\n');
+
+const files = (await fs.readdir(path + "/pages/templates/", {withFileTypes: true})).filter(entry => !entry.isDirectory()).map(entry => entry.name);
+
+console.log("Rendering EJs templates :", ...(files));
+//lines.forEach(line => renderFile(line));
+await Promise.all(files.map(file => renderFile(file)));
 console.log("Finished rendering");
